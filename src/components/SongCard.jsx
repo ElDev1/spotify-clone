6abd1e387134b6d52145286611908d472c9d7373
Devi Amaolo <deviamaolo@gmail.com>
2022-11-22 12:17:11 -0300

    create playPause button

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,8 +4,15 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i}) => {
-  const activeSong = 'Test';
+const SongCard = ({ song, i, isPlaying, activeSong, data}) => { 
+
+  const handleaPauseClik = () => {
+
+  }
+
+  const handlePlayClick = () => {
+
+  }
 
   return (
     <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup
@@ -13,7 +20,13 @@ const SongCard = ({ song, i}) => {
       <div className='relative w-full h-56 group'>
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex 
           ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
-            <PlayPause />
+            <PlayPause 
+              song={song}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handleaPause={handleaPauseClik}
+              handlePlay={handlePlayClick}
+            />
         </div>
         <img alt='song_img' src={song.images?.coverart}/>
       </div>
