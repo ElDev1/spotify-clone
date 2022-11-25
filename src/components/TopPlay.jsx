372268c7de0e5743daa6a01dfe19211a9e1ab7f3
Devi Amaolo <deviamaolo@gmail.com>
2022-11-25 01:46:01 -0300

    implement topPlay functionality

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,11 +11,29 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const TopChartCard = ({ song, i}) => (
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClik, handlePlayClick }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c436e] py-2
     p-4 rounded-lg cursor-pointer mb-2"
   >
-    {song.title}
+    <h3 className="fot-bold tex-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/song/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/song/${song?.artists[0].adamid}`}>
+          <p className="text-aase text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
+    <PlayPause 
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClik}
+      handlePlay={handlePlayClick}
+    />
   </div>
 )
 
@@ -32,11 +50,11 @@ const TopPlay = () => {
 
   const topPlays = data?.slice(0, 5); 
 
-  const handleaPauseClik = () => {
+  const handlePauseClik = () => {
     dispatch(playPause(false));
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i}));
     dispatch(playPause(true));
   };
@@ -61,9 +79,49 @@ const TopPlay = () => {
               key={song.key}
               song={song}
               i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePlayClick={() => handlePlayClick(song, i)}
+              handlePauseClik={handlePauseClik}
             />
           ))}
         </div>
+      </div>
+      
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+            <h2 className="text-white font-bold text-2x1">
+              Top Artists
+            </h2>
+            <Link to="/top-artists">
+              <p className="text-gray-300 text-base cursor-pointer">
+                See more
+              </p>
+            </Link>  
+          </div>
+
+          <Swiper
+            slidesPerView="auto"
+            spaceBetween={15}
+            freeMode
+            centeredSlides
+            centeredSlidesBounds
+            modules={[FreeMode]}
+            className="mt-4"
+          >
+            {topPlays?.map((song,  i) => (
+              <SwiperSlide
+                key={song?.key}
+                style={{width: '25%', height: "auto"}}
+                className="shadow-lg rounded-full animate-slideright"
+              >
+                <Link to={`/artist/${song?.artists[0].adamid}`}> 
+                  <img src={song?.images.background} alt="name" className="rounded-full w-full object-cover"
+                  />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
       </div>
     </div>
   )
