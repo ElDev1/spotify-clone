ed8910cec81f63c982af055c3540278b5297c53c
Devi Amaolo <deviamaolo@gmail.com>
2022-11-30 02:43:56 -0300

    add getGenre endpoint

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,13 +1,13 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 
-import { Error, Loader, SongCard } from '../components'
-import { genres } from '../assets/constants'
-
-import { useGetTopChartsQuery } from '../redux/services/shazamCore'
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+import { selectGenreListId } '../redux/features/playerSlice.js';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
     const dispatch = useDispatch();
-    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
 
     const { data, isFetching, error } = useGetTopChartsQuery()
     const demoGenre = 'pop'
@@ -21,8 +21,8 @@ const Discover = () => {
             sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-exl text-white text-left">Discover {demoGenre}</h2>
                 <select
-                    onChange={() => {}}
-                    value=""
+                    onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+                    value={genreListId || "pop"}
                     className="bg-black text-gray-300 p3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
                 >
                     {genres.map(genre => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
